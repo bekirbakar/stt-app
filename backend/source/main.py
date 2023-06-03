414e757bdb92337456c2e5624f3bf49ab82a829a
bekirbakar <bekirbakar@icloud.com>
2023-06-03 13:40:07 +0300

    Allow passing json as string too.

--- backend/source/main.py
+++ backend/source/main.py
@@ -10,17 +10,25 @@ logging.basicConfig(level=logging.INFO,
 
 
 class Parameters:
-    def __init__(self, path_to_parameters_file) -> None:
+    def __init__(self, input_parameters) -> None:
         self.logger = logging.getLogger(__name__)
-        try:
-            with open(path_to_parameters_file, 'r') as file:
-                parameters = json.load(file)
-        except FileNotFoundError:
-            self.logger.error(f'No such file: {path_to_parameters_file}')
-            sys.exit(1)
-        except json.JSONDecodeError:
-            self.logger.error(f'Invalid JSON file: {path_to_parameters_file}')
-            sys.exit(1)
+
+        if os.path.isfile(input_parameters):
+            try:
+                with open(input_parameters, 'r') as file:
+                    parameters = json.load(file)
+            except FileNotFoundError:
+                self.logger.error(f'No such file: {input_parameters}')
+                sys.exit(1)
+            except json.JSONDecodeError:
+                self.logger.error(f'Invalid JSON file: {input_parameters}')
+                sys.exit(1)
+        else:
+            try:
+                parameters = json.loads(input_parameters)
+            except json.JSONDecodeError:
+                self.logger.error(f'Invalid JSON string: {input_parameters}')
+                sys.exit(1)
 
         self.engine = parameters.get('engine', None)
         self.model_path = parameters.get('modelPath', None)
@@ -33,7 +41,11 @@ class Parameters:
 class EngineInference:
     def __init__(self, parameters) -> None:
         self.parameters = parameters
-        sys.path.insert(0, os.path.join(os.getcwd(), self.parameters.engine))
+        sys.path.insert(
+            0,
+            os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         self.parameters.engine)
+        )
 
         from engine import Engine
         self.engine = Engine(model_path=self.parameters.model_path,
@@ -47,11 +59,11 @@ class EngineInference:
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        logging.error('Usage: python script.py <path_to_parameters_file>')
+        logging.error('Usage: python script.py <input_parameters>')
         sys.exit(1)
 
-    path_to_parameters_file = sys.argv[1]
-    parameters = Parameters(path_to_parameters_file)
+    input_parameters = sys.argv[1]
+    parameters = Parameters(input_parameters)
 
     engine_inference = EngineInference(parameters)
     print(engine_inference.run(parameters.path_to_audio_file))
